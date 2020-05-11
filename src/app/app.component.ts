82201fdce4765969b2902f2edff583e925516262
Sean Blessing <snblessing@gmail.com>
2020-05-11 11:10:32 -0400

    basic calc functions implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-calculator';
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+
+  add() {
+    this.result = this.number1 + this.number2;
+  }
+  subtract() {
+    this.result = this.number1 - this.number2;
+  }
+  multiply() {
+    this.result = this.number1 * this.number2;
+  }
+  divide() {
+    this.result = this.number1 / this.number2;
+  }
 }
